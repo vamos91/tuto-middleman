c4be4bf8eab6ca83071256a5114da12afe63b292
nicolas acard <acardnicolas@hotmail.com>
2016-05-27 14:32:13 +0200

    home-page

--- skypicture/config.rb
+++ skypicture/config.rb
@@ -31,9 +31,9 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-["anne", "seb", "romain"].each do |name|
-  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
-end
+# ["anne", "seb", "romain"].each do |name|
+#   proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+# end
 
 ###
 # Helpers
@@ -53,6 +53,8 @@ end
 #     "Helping"
 #   end
 # end
+activate :bootstrap_navbar
+
 
 set :css_dir, 'stylesheets'
 
