7d3ac0cff8fe341d082a556410bdc85d0855ce09
nicolas acard <acardnicolas@hotmail.com>
2016-06-03 17:03:04 +0200

    first deploy

--- skypicture/source/javascripts/navbar.js
+++ skypicture/source/javascripts/navbar.js
@@ -1,23 +1,25 @@
-// (function ($) {
-//   $(document).ready(function(){
+(function ($) {
+  $(document).ready(function(){
 
-  // hide .navbar first
-  // $(".navbar").fadeTo( "fast", 0.33 );
+  //hide .navbar first
+  //$(".navbar-sup").fadeTo( "fast", 1 );
 
-//   // fade in .navbar
-//   $(function () {
-//     $(window).scroll(function () {
-//             // set distance user needs to scroll before we fadeIn navbar
-//       if ($(this).scrollTop() > 100) {
-//         $('.navbar').fadeTo( "fast", 0.8 );
-//       } else {
-//         $('.navbar').fadeOut();
-//       }
-//     });
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+      if ($(this).scrollTop() > 100) {
+        $('.navbar-sup').hide();
+      }
+      if ($(this).scrollTop() < 100) {
+        $('.navbar-sup').fadeIn();
+        }
+
+    });
 
 
-//   });
+  });
 
-// });
-//   }(jQuery));
-// // below will control the scrollspy
+});
+  }(jQuery));
+// below will control the scrollspy
