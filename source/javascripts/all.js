b44eea3c7304426ee2b37513efe257abee640e2d
nicolas acard <acardnicolas@hotmail.com>
2016-06-13 17:47:22 +0200

    change root directory

---
+++ source/javascripts/all.js
@@ -0,0 +1,35 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
+//
+//
+  /* Demo purposes only */
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/javascripts/navbar.js
@@ -0,0 +1,25 @@
+(function ($) {
+  $(document).ready(function(){
+
+  //hide .navbar first
+  //$(".navbar-sup").fadeTo( "fast", 1 );
+
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+      if ($(this).scrollTop() > 100) {
+        $('.navbar-sup').hide();
+      }
+      if ($(this).scrollTop() < 100) {
+        $('.navbar-sup').fadeIn();
+        }
+
+    });
+
+
+  });
+
+});
+  }(jQuery));
+// below will control the scrollspy
