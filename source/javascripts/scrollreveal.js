bd91d67e3928ab06b418831296a950e0f1fa31e3
nicolas acard <acardnicolas@hotmail.com>
2016-11-02 15:14:51 +0100

    reveal

--- source/javascripts/scrollreveal.js
+++ source/javascripts/scrollreveal.js
@@ -21,7 +21,8 @@ sr.reveal('.wrapper-thumb', {delay: 600, reset: false});
 sr.reveal('.feature1', {delay: 600, reset: false});
 sr.reveal('.text1', {delay: 600, reset: false});
 sr.reveal('.text2', {delay: 600, reset: false});
-sr.reveal ('.wrapper-price', {delay: 600, reset: false})
+sr.reveal('.wrapper-price', {delay: 600, reset: false});
+sr.reveal('.thumbnail', {delay: 600, reset: false});
 });
   }(jQuery));
 // below will control the scrollspy
