6595b43c5817ea405dd0ef5cd0b2433d3edc9955
nicolas <acardnicolas@hotmail.com>
2018-05-02 18:34:49 +0200

    update front

--- source/javascripts/scrollreveal.js
+++ source/javascripts/scrollreveal.js
@@ -13,7 +13,8 @@ sr.reveal('.fadeDown', {origin: 'top', delay: 600, reset: false});
 sr.reveal('.fadeLeft', {origin: 'left',delay: 600, reset: false});
 sr.reveal('.fadeRight', {origin: 'bottom', delay: 600, reset: false});
 sr.reveal('.opacity', {opacity: 0, delay: 600, reset: false});
-sr.reveal('.fadeLeftLong', {origin: 'left', distance: '100px',duration: 1000, reset: false});
+sr.reveal('.fadeLeftLong', {origin: 'left', delay: 600, reset: false});
+sr.reveal('.fadeRightLong', {origin: 'right', distance: '100px',duration: 1000, reset: false});
 
 
 });
